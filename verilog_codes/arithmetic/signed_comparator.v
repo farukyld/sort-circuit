d4393b9aeca251ca244ddceda7a13e34e4089e53
farukyld <123uzunince123@gmail.com>
2023-07-17 18:48:05 +0300

    added neccessary arithmetic units for the datapath

---
+++ verilog_codes/arithmetic/signed_comparator.v
@@ -0,0 +1,11 @@
+module signed_comparator #(parameter DATA_WIDTH = 8) (
+    input wire signed [DATA_WIDTH-1:0] a,
+    input wire signed [DATA_WIDTH-1:0] b,
+    output wire lt,
+    output wire eq,
+    output wire gt
+);
+    assign eq = (a == b);
+    assign lt = (a < b);
+    assign gt = (a > b);
+endmodule
