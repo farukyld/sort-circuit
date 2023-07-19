f314d8e3cbe6117782ef91fb95e61b5757831f5d
farukyld <123uzunince123@gmail.com>
2023-07-19 15:20:50 +0300

    modified random generator to use rst_n

--- verilog_codes/arithmetic/random_generator.v
+++ verilog_codes/arithmetic/random_generator.v
@@ -4,16 +4,20 @@ module random_generator #(
     parameter DATA_WIDTH = 32
 )(
     input wire clk,
+    input wire rst_n,
     input wire enable,
     output reg [DATA_WIDTH-1:0] rnd
 );
 
     // Random seed
-    reg [DATA_WIDTH-1:0] seed = 123456;
+    localparam RANDOM_SEED = 123456;
+    reg [DATA_WIDTH-1:0] seed = RANDOM_SEED;
 
     // Pseudo-random number generation using a simple multiplication and modulo approach
-    always @(posedge clk) begin
-        if (enable) begin
+    always @(posedge clk or negedge rst_n)  begin
+        if (~rst_n)
+            seed = RANDOM_SEED;
+        else if (enable) begin
             seed <= seed * 1103515245 + 12345; // Commonly used coefficients
             rnd <= seed;
         end
